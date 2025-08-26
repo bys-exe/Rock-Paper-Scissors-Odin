1d1d6e624e74ffdd551fd461830f2121c0ed53dd
bys-exe <batchuyugasai@gmail.com>
2025-08-26 20:52:44 +0530

    Add loop system and final decision
    added loop which iterates 5 times and also made the winning system

--- script.js
+++ script.js
@@ -29,39 +29,53 @@ function getHumanChoice(){
     }
     return HumanChoice;
 }
-let HumanChoice=getHumanChoice();
-let CPUchoice=getComputerChoice();
 let humanScore=0;
 let computerScore=0;
 function playRound(HumanChoice,CPUchoice){
-    let humanScore=0;
-    let computerScore=0;
     if (HumanChoice==CPUchoice){
         console.log('Its a Draw! No one gets a point.');
     }
-    if (HumanChoice=='rock' && CPUchoice=='paper') {
+    else if (HumanChoice=='rock' && CPUchoice=='paper') {
         console.log('Paper beats rock, CPU gets point.');
         computerScore++
     }
-    if (HumanChoice=='rock' && CPUchoice=='scissor') {
+    else if (HumanChoice=='rock' && CPUchoice=='scissor') {
         console.log('Rock beats scissor, Human gets point.');
         humanScore++
     }
-    if (HumanChoice=='paper' && CPUchoice=='rock') {
+    else if (HumanChoice=='paper' && CPUchoice=='rock') {
         console.log('Paper beats rock, Human gets point.');
         humanScore++
     }
-    if (HumanChoice=='paper' && CPUchoice=='scissor') {
+    else if (HumanChoice=='paper' && CPUchoice=='scissor') {
         console.log('Scissor beats paper, CPU gets point.');
         computerScore++
     }
-    if (HumanChoice=='scissor' && CPUchoice=='rock') {
+    else if (HumanChoice=='scissor' && CPUchoice=='rock') {
         console.log('Rock beats scissor, CPU gets point.');
         computerScore++
     }
-    if (HumanChoice=='scissor' && CPUchoice=='paper') {
+    else if (HumanChoice=='scissor' && CPUchoice=='paper') {
         console.log('scissor beats paper, Human gets point.');
         humanScore++
     }
 }
-playRound(HumanChoice,CPUchoice);
+function playGame(){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
+for (i=1; i<6; i++){
+    playGame();
+    console.log('Your Score: ' +humanScore);
+    console.log('CPU Score: ' +computerScore);
+}
+if (humanScore>computerScore){
+    console.log('You Win!');
+}
+else if (humanScore<computerScore){
+    console.log('CPU Wins! Better Luck next time.');
+}
+else if (humanScore==computerScore){
+    console.log('It was a draw!');
+}
