c1dfb55354129f402f55b50f9353cf23c80829f9
bys-exe <batchuyugasai@gmail.com>
2025-08-26 19:07:30 +0530

    Add playround function to script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function getComputerChoice(){
     else if (no===2){
         CPUchoice='scissor';
     }
+    return CPUchoice;
 }
 function getHumanChoice(){
     no=prompt('Choose a move from the choices below:\nType 0 for Rock\nType 1 for Paper\nType 2 for Scissor');
@@ -26,4 +27,41 @@ function getHumanChoice(){
     else if (no==2){
         HumanChoice='scissor';
     }
+    return HumanChoice;
 }
+let HumanChoice=getHumanChoice();
+let CPUchoice=getComputerChoice();
+let humanScore=0;
+let computerScore=0;
+function playRound(HumanChoice,CPUchoice){
+    let humanScore=0;
+    let computerScore=0;
+    if (HumanChoice==CPUchoice){
+        console.log('Its a Draw! No one gets a point.');
+    }
+    if (HumanChoice=='rock' && CPUchoice=='paper') {
+        console.log('Paper beats rock, CPU gets point.');
+        computerScore++
+    }
+    if (HumanChoice=='rock' && CPUchoice=='scissor') {
+        console.log('Rock beats scissor, Human gets point.');
+        humanScore++
+    }
+    if (HumanChoice=='paper' && CPUchoice=='rock') {
+        console.log('Paper beats rock, Human gets point.');
+        humanScore++
+    }
+    if (HumanChoice=='paper' && CPUchoice=='scissor') {
+        console.log('Scissor beats paper, CPU gets point.');
+        computerScore++
+    }
+    if (HumanChoice=='scissor' && CPUchoice=='rock') {
+        console.log('Rock beats scissor, CPU gets point.');
+        computerScore++
+    }
+    if (HumanChoice=='scissor' && CPUchoice=='paper') {
+        console.log('scissor beats paper, Human gets point.');
+        humanScore++
+    }
+}
+playRound(HumanChoice,CPUchoice);
