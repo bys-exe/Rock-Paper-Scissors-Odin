75ad9322bfc3b04a4f69f27f31045e759de25067
bys-exe <batchuyugasai@gmail.com>
2025-08-26 17:37:43 +0530

    Add computerchoice function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+function getComputerChoice(){
+    let no=getRandomInt(3);
+    let choice=0;
+    if (no===0){
+        choice='rock';
+    }
+    else if (no===1){
+        choice='paper';
+    }
+    else if (no===2){
+        choice='scissor';
+    }
+}
+function getHumanChoice{
+    
+}
